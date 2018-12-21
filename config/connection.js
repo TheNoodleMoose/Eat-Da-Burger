3e45c84ca07e01f77c1fe2c0262c23aae2d6c158
TheNoodleMoose <cjh605@gmail.com>
2018-12-21 14:18:43 -0500

    Added connection to Jaws DB

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,11 @@ const mysql = require("mysql");
 
 //Setup our connection to our database
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+  host: "ou6zjjcqbi307lip.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "	wqf1bx9unumbo6un",
+  password: "mnoo9bqzwgg0b2nd",
+  database: "ob6vfuxy3lgu5r9b"
 });
 
 connection.connect(err => {
